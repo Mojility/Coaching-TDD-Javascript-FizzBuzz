ea7bd29e80e055079ca217b38556c9b4a7d9a472
Stacey Vetzal <stacey@vetzal.com>
2016-04-23 09:57:27 -0400

    fixing var

--- spec/Emitter-spec.js
+++ spec/Emitter-spec.js
@@ -1,14 +1,14 @@
-Emitter = require("../Emitter");
+var Emitter = require("../Emitter");
 
 describe("Emitter", function() {
-  var h = new Emitter();
+    var h = new Emitter();
 
-  it("should emit simple numbers", function() {
-    expect(h.output(1)).toEqual("1");
-    expect(h.output(2)).toEqual("2");
-  });
+    it("should emit simple numbers", function() {
+        expect(h.output(1)).toEqual("1");
+        expect(h.output(2)).toEqual("2");
+    });
 
-  it("should emit Fizz", function() {
-    expect(h.output(3)).toEqual("Fizz");
-  });
+    it("should emit Fizz", function() {
+        expect(h.output(3)).toEqual("Fizz");
+    });
 });
