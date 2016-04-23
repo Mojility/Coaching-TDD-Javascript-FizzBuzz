7309d96842147bb12ba37c992349c88a60aefc5a
Stacey Vetzal <stacey@vetzal.com>
2016-04-23 10:28:16 -0400

    more cleanup

--- spec/Logic-spec.js
+++ spec/Logic-spec.js
@@ -2,20 +2,19 @@ var Logic = require("../Logic");
 
 describe("Logic", function() {
 
-  function check(divisor, output) {
-    var l = new Logic(divisor, output);
-    expect(l.output(divisor)).toEqual(output);
-    expect(l.output(divisor+1)).toEqual("");
-    expect(l.output(divisor-1)).toEqual("");
-    expect(l.output(divisor*2)).toEqual(output);
-  }
+    function check(divisor, output) {
+        var l = new Logic(divisor, output);
+        expect(l.output(divisor)).toEqual(output);
+        expect(l.output(divisor + 1)).toEqual("");
+        expect(l.output(divisor - 1)).toEqual("");
+        expect(l.output(divisor * 2)).toEqual(output);
+    }
 
-  it("should respond for Fizz", function() {
-    check(3, "Fizz");
-  });
+    it("should respond for Fizz", function() {
+        check(3, "Fizz");
+    });
 
-  it("should respond for Buzz", function() {
-    check(5, "Buzz");
-  });
+    it("should respond for Buzz", function() {
+        check(5, "Buzz");
+    });
 });
-
