6257dba4b6a79d56bfabfc9dd997c3c8372df940
Stacey Vetzal <stacey@vetzal.com>
2016-04-23 09:55:09 -0400

    cleaning up

--- Emitter.js
+++ Emitter.js
@@ -1,24 +1,21 @@
 module.exports = function() {
-  this.main = function() {  
-  }
 
-  this.output = function(input) {
-    var stringOutput = "";
+    this.output = function(input) {
+        var stringOutput = "";
 
-    if(input % 3 == 0)
-      stringOutput += "Fizz";
+        if (input % 3 == 0)
+            stringOutput += "Fizz";
 
-    if(input % 5 == 0)
-      stringOutput += "Buzz";
+        if (input % 5 == 0)
+            stringOutput += "Buzz";
 
-    if(input % 7 == 0)
-      stringOutput += "Woof";
+        if (input % 7 == 0)
+            stringOutput += "Woof";
 
-    if (!stringOutput)
-      stringOutput += input;
+        if (!stringOutput)
+            stringOutput += input;
 
-    return stringOutput;
-  }
-}
+        return stringOutput;
+    };
 
-
+};
